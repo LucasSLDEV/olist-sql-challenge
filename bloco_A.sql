ce8f56b14348dc36068c0215418c6ec9db1e0fea
LucasSLDEV <100497487+LucasSLDEV@users.noreply.github.com>
2026-09-22 09:47:48 -0300

    Restore SQL queries in bloco_A.sql
    ALINHAMENTO DO CODIGO FINAL
    
    Signed-off-by: LucasSLDEV <100497487+LucasSLDEV@users.noreply.github.com>

--- bloco_A.sql
+++ bloco_A.sql
@@ -8,7 +8,6 @@ WHERE order_status = 'delivered'
 ORDER BY order_delivered_customer_date DESC
 LIMIT 20;
 
-
 -- 2. Listar todos os produtos de uma categoria específica (usando a tabela de tradução para filtrar pelo nome em português).
 SELECT 
     p.product_id,
@@ -23,11 +22,9 @@ JOIN product_category_name_translation t
   ON p.product_category_name = t.product_category_name
 WHERE p.product_category_name = 'perfumaria';
 
-
 -- 3. Listar os métodos de pagamento distintos utilizados na base (SELECT DISTINCT payment_type).
 SELECT DISTINCT payment_type
 FROM olist_order_payments_dataset;
-
 
 -- 4. Listar os produtos com peso (product_weight_g) acima de 10kg, ordenados do mais pesado para o mais leve.
 SELECT 
